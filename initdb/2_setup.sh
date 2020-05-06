67e7238b91db865b9305c58246ea3b63086e0502
tamagoyaki-yakitamago <yuki.takeda.6e0702@outlook.jp>
2020-05-06 12:18:53 +0000

    emailをPRIMARY KEYに変更

--- initdb/2_setup.sh
+++ initdb/2_setup.sh
@@ -1,9 +1,8 @@
 #!/bin/bash
 psql -U postgres -d test_db << "EOSQL"
 create table test_table (
-    id serial PRIMARY KEY,
     username varchar(255) not null,
-    email varchar(255) not null,
+    email varchar(255) PRIMARY KEY not null,
     password varchar(255) not null,
     created_at timestamp not null,
     delete_at timestamp not null);
